a474917f977728751d4fb9624510df054b075b47
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-04-07 21:51:17 +0300

    new

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -1,14 +1,13 @@
 package runners;
 
-import io.cucumber.junit.*;
-import org.junit.runner.*;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {
-                "pretty",
-                "html:target/cucumber",
-                "json:target/cucumber.json",
+        plugin = {"json:target/cucumber.json",
+                "html:target/cucumber-report.html",
                 "me.jvt.cucumber.report.PrettyReports:target/cucumber",
         },
         features = "src/test/resources/features",
