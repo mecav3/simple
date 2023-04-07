a93c5d1ec0e3dea3ad9bfe9fd12abd069dd861d7
mecav3 <74742815+mecav3@users.noreply.github.com>
2023-04-07 22:26:57 +0300

    new

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         plugin = {"json:target/cucumber.json",
                 "html:target/cucumber-report.html",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber",
+                //"me.jvt.cucumber.report.PrettyReports:target/cucumber",
         },
         features = "src/test/resources/features",
         glue = "step_definitions",
